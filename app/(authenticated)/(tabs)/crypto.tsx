4a63a223bc92a50ec08ce145881d124cf22930f0
Bilal Naeem <94391016+billu2002@users.noreply.github.com>
2024-04-09 01:34:53 +0500

    Chart is now implemented

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,24 +1,26 @@
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
-import { useQuery } from '@tanstack/react-query';
-import { Currency } from '@/interfaces/crypto';
-import { Link } from 'expo-router';
-import { useHeaderHeight } from '@react-navigation/elements';
-import Colors from '@/constants/Colors';
-import { defaultStyles } from '@/constants/Styles';
-import { Ionicons } from '@expo/vector-icons';
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { Currency } from "@/interfaces/crypto";
+import { Link } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
   const headerHeight = useHeaderHeight();
 
   const currencies = useQuery({
-    queryKey: ['listings'],
-    queryFn: () => fetch('/api/listings').then((res) => res.json()),
+    queryKey: ["listings"],
+    queryFn: () => fetch("/api/listings").then((res) => res.json()),
   });
 
-  const ids = currencies.data?.map((currency: Currency) => currency.id).join(',');
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(",");
 
   const { data } = useQuery({
-    queryKey: ['info', ids],
+    queryKey: ["info", ids],
     queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
     enabled: !!ids,
   });
@@ -26,27 +28,50 @@ const Page = () => {
   return (
     <ScrollView
       style={{ backgroundColor: Colors.background }}
-      contentContainerStyle={{ paddingTop: headerHeight }}>
+      contentContainerStyle={{ paddingTop: headerHeight }}
+    >
       <Text style={defaultStyles.sectionHeader}>Latest Crypot</Text>
       <View style={defaultStyles.block}>
         {currencies.data?.map((currency: Currency) => (
           <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
-            <TouchableOpacity style={{ flexDirection: 'row', gap: 14, alignItems: 'center' }}>
-              <Image source={{ uri: data?.[currency.id].logo }} style={{ width: 40, height: 40 }} />
+            <TouchableOpacity
+              style={{ flexDirection: "row", gap: 14, alignItems: "center" }}
+            >
+              {data && data[currency.id] && data[currency.id].logo && (
+                <Image
+                  source={{ uri: data[currency.id].logo }}
+                  style={{ width: 40, height: 40 }}
+                />
+              )}
+
               <View style={{ flex: 1, gap: 6 }}>
-                <Text style={{ fontWeight: '600', color: Colors.dark }}>{currency.name}</Text>
+                <Text style={{ fontWeight: "600", color: Colors.dark }}>
+                  {currency.name}
+                </Text>
                 <Text style={{ color: Colors.gray }}>{currency.symbol}</Text>
               </View>
-              <View style={{ gap: 6, alignItems: 'flex-end' }}>
+              <View style={{ gap: 6, alignItems: "flex-end" }}>
                 <Text>{currency.quote.EUR.price.toFixed(2)} €</Text>
-                <View style={{ flexDirection: 'row', gap: 4 }}>
+                <View style={{ flexDirection: "row", gap: 4 }}>
                   <Ionicons
-                    name={currency.quote.EUR.percent_change_1h > 0 ? 'caret-up' : 'caret-down'}
+                    name={
+                      currency.quote.EUR.percent_change_1h > 0
+                        ? "caret-up"
+                        : "caret-down"
+                    }
                     size={16}
-                    color={currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red'}
+                    color={
+                      currency.quote.EUR.percent_change_1h > 0 ? "green" : "red"
+                    }
                   />
                   <Text
-                    style={{ color: currency.quote.EUR.percent_change_1h > 0 ? 'green' : 'red' }}>
+                    style={{
+                      color:
+                        currency.quote.EUR.percent_change_1h > 0
+                          ? "green"
+                          : "red",
+                    }}
+                  >
                     {currency.quote.EUR.percent_change_1h.toFixed(2)} %
                   </Text>
                 </View>
